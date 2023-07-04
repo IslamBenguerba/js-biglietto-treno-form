791e1eb452a495622f5beae918395267ca114fc3
Islam.Benguerba <islam.benguerba@gmail.com>
2023-07-04 19:17:24 +0200

    Non finito

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 const bottone = document.getElementById('create')
-bottone.addEventListener('click', calcolo);
+bottone.addEventListener("click", calcolo)
 function calcolo() {
+    const card = document.getElementById('card')
+    card.classList.remove('d-none')
     const utente = document.getElementById('user').value
     const distanza = document.getElementById('distanza').value
-    const eta = document.getElementById('anni').valueconst 
-    const finaluser = document.getElementById('nomecliente').innerHTML = `${utente}`
-    
-    let costobiglietto= distanza * 0.21;
-    if (eta <18){
+    const eta = document.getElementById('anni').value
+    let costobiglietto = distanza * 0.21;
+    console.log(eta)
+    if (eta < 18) {
         let sconto = ((costobiglietto * 20) / 100)
         let prezzofinale = costobiglietto - sconto;
-        return prezzofinale
-    } else if(eta >=65){
+        let prezzo = document.getElementById('prezzo').innerHTML = `${prezzofinale}`
+    } else if (eta >= 65) {
         let sconto = ((costobiglietto * 40) / 100)
         let prezzofinale = costobiglietto - sconto;
-        return prezzofinale
+        let prezzo = document.getElementById('prezzo').innerHTML = `${prezzofinale}`
+    } else{
+        let sconto = 0
+        prezzofinale = costobiglietto
+        let prezzo = document.getElementById('prezzo').innerHTML = `${prezzofinale}`
     }
-    return prezzofinale
+    let cliente = document.getElementById('nomecliente').innerHTML = `${utente}`
+    
+} 
+
+const btnAnnulla = document.getElementById('biglietto')
+btnAnnulla.addEventListener("click", annulla)
+function annulla() {
+    const card = document.getElementById('card')
+    card.classList.add('d-none')
 }
-
-const finalprice = document.getElementById('prezzo').innerHTML = `${prezzofinale}`
-
